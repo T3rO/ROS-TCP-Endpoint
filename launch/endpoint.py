b5f5efa289b58b60508bc9a3053297595df147ff
Tristan Jacobs <tristan-jacobs@hotmail.com>
2025-03-19 21:51:39 +0100

    Use ip and port arguments in launch file

--- launch/endpoint.py
+++ launch/endpoint.py
@@ -1,15 +1,33 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+
+    ros_ip = DeclareLaunchArgument(
+        "tcp_ip", 
+        default_value="0.0.0.0"
+    )
+
+    ros_tcp_port = DeclareLaunchArgument(
+        "tcp_port", 
+        default_value="10000"
+    )
+
     return LaunchDescription(
         [
+            ros_ip,
+            ros_tcp_port,
             Node(
                 package="ros_tcp_endpoint",
                 executable="default_server_endpoint",
                 emulate_tty=True,
-                parameters=[{"ROS_IP": "0.0.0.0"}, {"ROS_TCP_PORT": 10000}],
+                parameters=[{
+                    "ROS_IP": LaunchConfiguration('tcp_ip'),
+                    "ROS_TCP_PORT": LaunchConfiguration('tcp_port')
+                }],
             )
         ]
     )
